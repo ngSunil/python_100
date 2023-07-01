a477d50c54151a6d699e5b6c04e00673381a325e
Sunil Biswakarma <sun.aec@gmail.com>
2023-07-02 00:15:39 +0530

    03 py

---
+++ 02.py
@@ -0,0 +1,7 @@
+# Write a program which accepts a sequence of comma-separated numbers from console and
+# generate a list and a tuple which contains every number.Suppose the following input is supplied to the program:
+x = input('Please enter the your comma separated numbers')
+# print(x.split(','))
+# print(tuple(x.split(',')))
+
+print(input().split(','))
---
+++ 03.py
@@ -0,0 +1,13 @@
+# Write a program that accepts a sequence of whitespace separated words as
+# input and prints the words after removing all duplicate words and sorting them alphanumerically.
+x = input('enter your sentence')
+sorted_list = sorted(list(set(x.split(' '))))
+# sentence = ''
+# for word in sorted_list:
+#     sentence += ' ' + word
+# print(sentence)
+print(' '.join(sorted_list))
+print('-'.join(['a', 'b', 'c']))
+print('sunil'.split())
+print([x for x in 'sunil'])
+print([*'sunil'])

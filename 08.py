50d6945f17b52590a79a31d5f5c17a01dfc24041
Sunil Biswakarma <sun.aec@gmail.com>
2023-07-02 19:02:15 +0530

    count frequency of words

---
+++ 08.py
@@ -0,0 +1,6 @@
+# Write a program to compute the frequency of the words from the input. The output should output after sorting the key alphanumerically.
+sentence = input('Please enter your sentence?.. ')
+sorted_sentence_list = sorted(set(sentence.split(" ")))
+original_sentence = sentence.split(" ")
+for word in sorted_sentence_list:
+    print(f'{word}: {sentence.count(word)}')
